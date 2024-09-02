55d5696516f1291143c24cdd5616ca6863d70e82
Dan Plyukhin <dplyukhin@gmail.com>
2024-09-02 12:48:59 -0500

    Wrapping channels with serialization

--- src/main/java/choral/examples/diffie-hellman/Alice.java
+++ src/main/java/choral/examples/diffie-hellman/Alice.java
@@ -3,16 +3,25 @@ package choral.examples.diffiehellman;
 import choral.channels.SymChannel_B;
 import choral.runtime.Media.ServerSocketByteChannel;
 import choral.runtime.serializers.JavaSerializer;
+import choral.runtime.WrapperByteChannel.WrapperByteChannel_B;
+import choral.runtime.SerializerChannel.SerializerChannel_B;
 import choral.examples.diffiehellman.DiffieHellman_Alice;
 
 public class Alice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws java.io.IOException {
 
         ServerSocketByteChannel listener =
             ServerSocketByteChannel.at( 
                 Config.ALICE_HOSTNAME, Config.BOB_PORT
             );
+
+        SerializerChannel_B channel = new SerializerChannel_B( 
+                new JavaSerializer(),
+				new WrapperByteChannel_B( 
+                    listener.getNext()
+                )
+        );
 
     }
 }
--- src/main/java/choral/examples/diffie-hellman/Bob.java
+++ src/main/java/choral/examples/diffie-hellman/Bob.java
@@ -2,10 +2,21 @@ package choral.examples.diffiehellman;
 
 import choral.channels.SymChannel_B;
 import choral.examples.diffiehellman.DiffieHellman_Bob;
+import choral.runtime.Media.SocketByteChannel;
+import choral.runtime.WrapperByteChannel.WrapperByteChannel_A;
+import choral.runtime.SerializerChannel.SerializerChannel_A;
 import choral.runtime.serializers.JavaSerializer;
 
 public class Bob {
 
     public static void main(String[] args) {
+        SerializerChannel_A channel = new SerializerChannel_A( 
+                new JavaSerializer(),
+				new WrapperByteChannel_A( 
+                    SocketByteChannel.connect(
+                        Config.ALICE_HOSTNAME, Config.BOB_PORT
+                    )
+                )
+		);
     }
 }
