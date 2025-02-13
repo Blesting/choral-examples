3c864af46b32336a26f69173efd67e6a92560e45
Blesting <blesteea@gmail.com>
2025-02-13 16:12:00 +0100

    updated benchmarking script

--- runBenchmarks.sh
+++ runBenchmarks.sh
@@ -1,14 +1,18 @@
 echo ""
+echo "== DownloadFile =="
+mvn exec:java -Dexec.mainClass="benchmarks.downloadfile.Main" -Dexec.args="1000 50 target benchmarks downloadfile"
+
+echo ""
 echo "== SendPackets =="
-mvn exec:java -Dexec.mainClass="benchmarks.sendpackets.Main" -Dexec.args="1000 target benchmarks sendpackets"
+mvn exec:java -Dexec.mainClass="benchmarks.sendpackets.Main" -Dexec.args="1000 50 target benchmarks sendpackets"
 
 echo ""
 echo "== DiffieHellman =="
-mvn exec:java -Dexec.mainClass="benchmarks.diffiehellman.Main" -Dexec.args="1000 target benchmarks diffiehellman"
+mvn exec:java -Dexec.mainClass="benchmarks.diffiehellman.Main" -Dexec.args="50 target benchmarks diffiehellman"
 
 echo ""
 echo "== Mergesort =="
-mvn exec:java -Dexec.mainClass="benchmarks.mergesort.Main" -Dexec.args="100 10 target benchmarks mergesort"
+mvn exec:java -Dexec.mainClass="benchmarks.mergesort.Main" -Dexec.args="100 50 target benchmarks mergesort"
 
 echo ""
 echo "== Quicksort =="
