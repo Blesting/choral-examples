ca4a28d4fc1f008b04f372a84f712e82b331d3b4
Blesting <blesteea@gmail.com>
2025-02-17 12:45:15 +0100

    added utils to splitandcombine

---
+++ src/main/java/choral/amend/splitandcombine/utils/Tasks.java
@@ -0,0 +1,10 @@
+package choral.amend.splitandcombine.utils;
+
+public class Tasks {
+    public Object first(){
+        return new Object();
+    }
+    public Object second(){
+        return new Object();
+    }
+}
